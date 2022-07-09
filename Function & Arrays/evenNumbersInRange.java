6eaead84216bb762990081aa8604399ddcb7ca09
Gulam Ansari <gulamansari57181@gmail.com>
2022-07-09 23:05:40 +0530

    Odd numbers in range leetcode problem

---
+++ Function & Arrays/evenNumbersInRange.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class evenNumbersInRange {
+    public static void main(String []args){
+Scanner sc=new Scanner(System.in);
+
+System.out.println("Enter lower value");
+int low=sc.nextInt();
+System.out.println("Enter highest value");
+int high =sc.nextInt();
+int ans=range(low,high);
+System.out.println("Total number of odd numbers between "+ low +" and "+ high + " is "+ ans );
+sc.close();
+
+
+
+    }
+    public static int range(int low , int high){
+        
+                
+               if(low%2 !=0 || high%2 !=0)
+                return (high-low)/2+1;
+                else
+                    return (high-low)/2;
+        
+    
+
+    }
+    
+}
